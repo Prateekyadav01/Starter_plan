654d108b80a1d665273263ba421c8520fb9bfe60
PrateekTechmatys <prateeky562@gmail.com>
2024-03-12 15:37:35 +0530

    CartSlice is done

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,5 +1,30 @@
+import { useState } from "react";
+import { MdDelete } from "react-icons/md";
+import { useDispatch } from "react-redux";
+import { remove } from "../redux/Slices/CartSlice";
 const CartItem = ({item}) => {
+  const [total , setTotal] = useState(0);
+  const dispatch = useDispatch();
+  const handleRemove =()=>{
+    dispatch(remove(item.id));
+  }
   return <div>
+    <div>
+        <div>
+          <img className="w-[200px] h-10" src={item.image} alt="no image" />
+        </div>
+        <div>
+          <h3>{item.title}</h3>
+          <p>{item.description}</p>
+          <div>
+            <p>Price: ${item.price}</p>
+            <div onClick={handleRemove}>
+            <MdDelete />
+            </div>
+          </div>
+        </div>
+    </div>
+    <hr />
     
   </div>;
 };
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,26 +1,36 @@
 import { useSelector } from "react-redux";
 import CartItem from "../components/CartItem";
+import { Link } from "react-router-dom";
 const Cart = () => {
   const cart = useSelector(state => state.cart);
   console.log(cart)
   return <div>
-    <div><h1>Cart</h1></div>
-     
-     <div>
+   
+
+    <div>
       {
-        cart && cart.length>0 ? (<div>
+        cart && cart.length > 0 ? (<div>
           {
-            cart.map((item)=>(
-              <CartItem key={item.id} item = {item}/>
+            cart.map((item) => (
+              <CartItem key={item.id} item={item} />
             ))
           }
-          </div>) :(
+          <div>
+            <h1>
+              Cart
+            </h1>
+            <p>Total</p>
+          </div>
+        </div>) : (
           <div>
             <h3>Cart is empty</h3>
+            <Link to={"/"}>
+              <button>Continue Shopping</button>
+            </Link>
           </div>
         )
       }
-     </div>
+    </div>
 
   </div>;
 };
